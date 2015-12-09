dbc5435fe8ae3d25ea167e8a75f3947fb45e0fb6
songzhiyuan <www.1319324030@qq.com>
2015-12-09 18:10:39 +0800

    list
    通过链表，对输入学生信息进行简化，避免复杂的工作。

---
+++ main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  list
+//
+//  Created by 20141105054 on 15/12/2.
+//  Copyright © 2015年 宋志远. All rights reserved.
+//
+
+#include <iostream>
+#include <string.h>
+struct student
+{
+    int age;
+    char name[100];
+    student *next;
+};
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    student *p,*q,*head;
+    head=new student;
+    head->next=0;
+    p=head;
+    for(i=0;i<4;i++)
+    {
+        q=new student;
+        p->next=q;
+        q->next=0;
+        scanf("%s %d",q->name,&(q->age));
+        p=q;
+    }
+    p=head->next;
+    while(p!=0)
+    {
+        printf("%s%d\n",p->name,p->age);
+        p=p->next;
+    }
+    
+    return 0;
+}
